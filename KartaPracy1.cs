64f64f378c5763e5a82f49c8794a931a523469fe
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-05 09:10:01 +0200

    Wykonanie KartaPracy1.pdf

---
+++ KartaPracy1.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartaPracy1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zad 1
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*a + Math.Pow(b,2));
+            */
+
+            // Zad 2
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine((a + b) * (a + b));
+            */
+
+            // Zad 3
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow((a-b), 3));
+            */
+            /*
+            Console.WriteLine("Zadanie 4");
+            Console.WriteLine("Wpisz liczbę a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wpisz liczbę b");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wpisz liczbę b");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b * c);
+
+
+            Console.WriteLine("Zadanie 5");
+            Console.WriteLine("Wpisz liczbę a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wpisz liczbę b");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(2 * (a + b) / 5);
+
+
+            Console.WriteLine("Zadanie 6");
+            Console.WriteLine("Wpisz brutto");
+            double brutto = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(b / 1.23);
+
+            Console.WriteLine("Zadanie 7");
+            Console.WriteLine("Wpisz liczbę a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wpisz liczbę b");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a % b);*/
+
+            // Console.ReadKey();
+
+        }
+    }
+}
