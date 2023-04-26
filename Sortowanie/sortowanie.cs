c64cce6d10ba62d0e05b5e3706ecfc84fcca19c8
PanInf <77333045+PanInf@users.noreply.github.com>
2023-04-26 11:01:40 +0200

    Dodanie MergeSort

--- Sortowanie/sortowanie.cs
+++ Sortowanie/sortowanie.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Transactions;
 // Sortowania przez porównanie
 
@@ -12,6 +13,7 @@ for (int i = 0; i < n; i++)
     T[i] = r.Next(1, 20);
     Console.Write(T[i] + " ");
 }
+Console.WriteLine("\n");
 
 // 1. Bąbelkowe (bubble sort)
 
@@ -63,28 +65,114 @@ for (int i = n-1; i > 0; i--)
 
 // 3. Przez wstawianie (insertion sort)
 
-int j, tempik;
+/*int j, tempik;
 for (int i = 1; i < n; i++)
 {
     tempik = T[i];
     j = i - 1;
     while (j >= 0 && tempik < T[j])
     {
-        T[j+1] = T[j];
+        T[j + 1] = T[j];
         j--;
     }
-    T[j+1] = tempik;
+    T[j + 1] = tempik;
+}*/
+
+// Sortowania w czasie liniowym - O(n)
+
+// 4. Przez zliczanie (counting sort)
+
+/*int[] L = new int[20];
+
+for (int i = 0; i < n; i++)
+{
+    L[T[i]]++;
 }
 
-// Sortowania w czasie liniowym
+for (int i = 0; i < L.Length; i++)
+{
+    Console.Write(L[i] + " ");
+}
 
-// 4. Przez zliczanie
+int j = 0;
+for (int i = 0; i < L.Length; i++)
+{
+    if (L[i] > 0)
+    {
+        for (int k = 0; k < L[i]; k++)
+        {
+            T[j] = i;
+            j++;
+        }   
+    }
+}
+
+Console.WriteLine("\n");*/
 
 // 5. Kubełkowe
+
+// pominięte - na później
 
 // Dziel i zwyciężaj 
 
 // 6. Przez scalanie
+
+
+void scalaj(int lewy, int prawy)  //0-3
+{
+    int[] pom = new int[n];
+    for (int a = 0; a < n; a++) pom[a] = T[a]; 
+    int i, i_lewy, i_prawy;
+    int srodek = (lewy + prawy) / 2;
+    i = lewy;  // indeks tabeli
+    i_lewy = lewy;
+    i_prawy = srodek + 1;
+
+    while (i_lewy <= srodek && i_prawy <= prawy)
+    {
+        if (pom[i_lewy] < pom[i_prawy])
+        {
+            T[i] = pom[i_lewy];
+            i_lewy++;
+        }
+        else
+        {
+            T[i] = pom[i_prawy];
+            i_prawy++;
+        }
+        i++;
+    }
+    if (i_lewy > srodek)
+        while (i_prawy <= prawy)
+        {
+            T[i] = pom[i_prawy];
+            i_prawy++;
+            i++;
+        }
+    else
+        while (i_lewy <= srodek)
+        {
+            T[i] = pom[i_lewy];
+            i_lewy++;
+            i++;
+        }
+}
+
+void sortuj(int lewy, int prawy)
+{
+    int srodek = (lewy + prawy) / 2;
+    if (lewy < srodek)
+    {
+        sortuj(lewy, srodek);
+    }
+    if (srodek + 1 < prawy)
+    {
+        sortuj(srodek + 1, prawy);
+    }
+    scalaj(lewy, prawy);
+}
+
+sortuj(0, n-1);
 
 // 7. Quicksort Hoare
 
@@ -92,6 +180,5 @@ for (int i = 1; i < n; i++)
 
 // Wyświetlenie posortowanej tablicy
 
-Console.WriteLine("\n");
 for (int i = 0; i < n; i++)
     Console.Write(T[i] + " ");
